cafea956028c4352da474ce1534a7bb601bb063c
gitTanzj <kalle.riit@gmail.com>
2025-02-18 14:02:07 +0200

    neljandani tehtud

--- src/App.js
+++ src/App.js
@@ -1,23 +1,59 @@
-import logo from './logo.svg';
+import { useState } from "react";
+
 import './App.css';
+import { createMockServer } from './createMockServer';
+
+if(process.env.NODE_ENV === 'development'){
+  createMockServer();
+}
 
 function App() {
+  const [query, setQuery] = useState('');
+  const [searchResults, setSearchResults] = useState([]);
+  const [selected, setSelected] = useState([]);
+
+  const inputChangeHandler = (event) => {
+    setQuery(event.target.value);
+  };
+
+  const buttonClickHandler = () => {
+    fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${query}&limit=5`)
+    .then((result) => {
+      return result.json()
+    })
+    .then((cities) => {
+      setSearchResults(cities.map((city) => ({
+        name: city.name,
+        country: city.country,
+        lat: city.lat,
+        lon: city.lon
+      })))
+    })
+  }
+
+  const selectCity = (city) => {
+    setSelected([city, ...selected]);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Weather Application</h1>
+      <input type="text" data-testid="search-input" onChange={inputChangeHandler}/>
+      <button data-testid="search-button" onClick={buttonClickHandler}>Search</button>
+
+      <div data-testid="search-results">
+        {searchResults.map((city) => <div key={`${city.lat}-${city.lon}`} onClick={() => selectCity(city)}>
+          {city.name}, {city.lat}, {city.lon}
+        </div>)}
+      </div>
+
+      <div data-testid="my-weather-list">
+        {selected && selected.map((city) => (
+          <div key={`${city.lat}-${city.lon}`}>
+            {city.name}
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,63 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, waitFor, act, within } from '@testing-library/react';
 import App from './App';
+import { createMockServer } from './createMockServer';
+import userEvent from '@testing-library/user-event';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+describe('Weather application tests', () => { 
+  let server;
+  beforeEach(() => {
+    server = createMockServer();
+  })
+  afterEach(() => {
+    server.shutdown();
+  })
+
+  it('renders weather application title', () => {
+    render(<App/>);
+    const linkElement = screen.getByText(/Weather Application/i);
+    expect(linkElement).toBeInTheDocument();
+  })
+
+  it('shows city search results', async () => {
+    render(<App/>);
+    const input = screen.getByTestId('search-input');
+    userEvent.type(input, 'Melbourne');
+
+    const button = screen.getByTestId('search-button');
+    userEvent.click(button);
+
+    await waitFor(() => expect(screen.getAllByText(/Melbourne/i).length).toEqual(5));
+  })
+
+  it('shows city search result details', async () => {
+    render(<App/>);
+
+    const input = screen.getByTestId('search-input');
+    userEvent.type(input, 'Melbourne');
+
+    const button = screen.getByTestId('search-button');
+    userEvent.click(button);
+
+    await waitFor(() => expect(screen.getAllByText(/Melbourne/i).length).toEqual(5))
+    expect(screen.getByText(/Melbourne, -37.8141705, 144.9655616/i)).toBeInTheDocument();
+  })
+
+  it('should add search result to my weather list', async () => {
+    render(<App/>);
+    const input = screen.getByTestId('search-input');
+    userEvent.type(input, 'Melbourne')
+
+    const button = screen.getByTestId('search-button');
+    userEvent.click(button);
+
+    await waitFor(() => expect(screen.getAllByText(/Melbourne/i).length).toEqual(5))
+
+    const selected = screen.getAllByText(/Melbourne/i)[3];
+    act(() => {
+      userEvent.click(selected);
+    })
+
+    expect(within(screen.getByTestId('my-weather-list')).getByText(/Melbourne/i)).toBeInTheDocument();
+  })
+})
+
